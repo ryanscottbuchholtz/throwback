11d0cafab898962755ca28fa627b77b6de3a4ad3
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2014-01-02 14:07:29 -0500

    initial commit. config and gem files set.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Throwback::Application.config.secret_key_base = '2eeccad55ac875a57021135d255b7ae5eec59fd04d1f1c589641367252f65b866494b6b8a7c05e7d6c0151c009cb3919522e2acd3275e7029afdf959b800deb1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Throwback::Application.config.session_store :cookie_store, key: '_throwback_session'
