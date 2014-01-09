c41d73ab4348266967a8e9f8b0f57a6ea0ef1f4b
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2014-01-09 07:50:55 -0500

    updates to admin ability to change categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -17,21 +17,38 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to @category, notice: "Successfully added category."
+      redirect_to categories_path, notice: "Successfully added category."
     else
       render :new
     end
   end
 
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
 
   protected
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
   end
 
   def authenticate_user
-    unless user_signed_in? and current_user_is_admin?
+    unless user_signed_in? and current_user.is_admin?
       raise ApplicationController::RoutingError.new('Not Found')
     end
   end
