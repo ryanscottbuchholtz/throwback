14ad11cfd80ae3a1e3875ff915e14573725d8f0c
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2014-01-27 10:33:48 -0500

    remove mailchimp api

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   end
 
   def mailchimp_status
-    @mailchimp_list_id = "99ea49d5d5"
+    @mailchimp_list_id = ENV["mailchimp_list_id"]
     @gb = Gibbon::API.new
       @gb.lists.subscribe({
       :id => @mailchimp_list_id,
--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
-Gibbon::API.api_key = "b50e24f8daacc0dab4e1f7bf961b9876-us3"
+Gibbon::API.api_key = ENV["mailchimp_api_key"]
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
